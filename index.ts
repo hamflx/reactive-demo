bc7af14794fd046bbf7c9e5e4759cd60982c7735
hamflx <a@hamflx.cn>
2022-09-07 18:36:02 +0800

    add constructor support

--- index.ts
+++ index.ts
@@ -1,8 +1,35 @@
+const Construct = Symbol("construct");
+
+type Construct = typeof Construct;
+
+type Property = string | symbol | Construct;
+type Paths = Property[];
+
+type Collector = (
+  paths: Paths,
+  value: any,
+  before: any,
+) => void;
+
+const magicProperties = ["prototype"];
+
+const chain =
+  (property: Property, collector: Collector): Collector =>
+  (paths, value, before) => collector([property, ...paths], value, before);
+
 const reactive = <T extends object>(
   obj: T,
-  collector: (paths: Array<string | symbol>, value: any, before: any) => void,
+  collector: Collector,
 ): T => {
   const proxyObject = new Proxy(obj, {
+    construct(target, argArray, newTarget) {
+      console.log("==> construct", target);
+      const obj = Reflect.construct(target as any, argArray, newTarget);
+      if (Object(obj) === obj) {
+        return reactive(obj, chain(Construct, collector));
+      }
+      return obj;
+    },
     set(target, property, value, receiver) {
       try {
         collector([property], value, Reflect.get(target, property, receiver));
@@ -11,14 +38,12 @@ const reactive = <T extends object>(
       }
       return Reflect.set(target, property, value, receiver);
     },
-    get(target, property, receiver) {
+    get(target, property: Property, receiver: any) {
       const value = Reflect.get(target, property, receiver);
-      if (Object(value) === value) {
-        return reactive(
-          value,
-          (paths, value, before) =>
-            collector([property, ...paths], value, before),
-        );
+      const isMagicProperty = typeof property === "string" &&
+        magicProperties.includes(property);
+      if (Object(value) === value && !isMagicProperty) {
+        return reactive(value, chain(property, collector));
       }
       return value;
     },
@@ -27,13 +52,13 @@ const reactive = <T extends object>(
 };
 
 const collect = () => {
-  const changes: Array<{ before: any; value: any; path: string }> = [];
+  const changes: Array<{ before: any; value: any; paths: Paths }> = [];
   const collector = (
-    paths: Array<string | symbol>,
+    paths: Paths,
     value: any,
     before: any,
   ) => {
-    changes.push({ value, before, path: paths.join(".") });
+    changes.push({ value, before, paths });
   };
   return {
     collector,
@@ -56,5 +81,15 @@ const o = reactive({
 
 const c = o.a.b.c;
 c.value = 5423;
+o.aaa = "hello typescript";
+
+const A = reactive(
+  class AImpl {
+    hello = "typescript";
+  },
+  changes.collector,
+);
+const a = new A();
+a.hello = "rust";
 
 console.log(changes.changes());
